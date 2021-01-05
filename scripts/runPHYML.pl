cdbb6d10f4f670679a0049d8c61a862afc61083b
Janet Young <jayoung@fredhutch.org>
2021-01-05 10:37:57 -0800

    add better runPYML.pl script

--- scripts/runPHYML.pl
+++ scripts/runPHYML.pl
@@ -4,9 +4,6 @@ use warnings;
 use strict;
 use Getopt::Long;
 
-## xx to do:
-# incorporate phyml-mpi option
-
 ####### runPHYMLonNucleotideAlignmentAndRemoveBranchLengths.pl alignmentFiles.fa
 
 #### e.g. for the file H2Aaln1.fa.trim49_294
@@ -34,6 +31,8 @@ my $removeBranchLengths = 1;
 
 my $phymlOptions = "--pinv e --alpha e -f e";
 
+my $numThreads = 1;
+
 ######## sbatch options:
 
 ### set this if on rhino and want to put out one job to each node.  Might want to switch to sbatch at some point??
@@ -50,6 +49,7 @@ GetOptions("type=s" => \$alnType,
            "model=s" => \$subModel,
            "options=s" => \$phymlOptions,
            "boots=i" => \$bootstrap,
+           "threads=i" => \$numThreads,
            "removeBlen=i" => \$removeBranchLengths,
            "sbatch=i" => \$use_sbatch,
            "walltime=s" => \$walltime,
@@ -65,6 +65,9 @@ if (($alnType ne "nt") & ($alnType ne "aa")) {
 }
 if (($alnType eq "aa") & ($subModel eq "GTR")) {
     die "\n\nterminating - you specified an amino acid alignment but did not change the default model from GTR, which is a nucleotide model. Try -model=JTT\n\n";
+}
+if (($numThreads > 1) & ($bootstrap == 0)) {
+    die "\n\nterminating - you specified >1 thread but you're not bootstrapping. Not recommended\n\n";
 }
 
 foreach my $file (@ARGV) {
@@ -86,11 +89,29 @@ foreach my $file (@ARGV) {
     $command .= "cd $treeDir ; ";
     # fasta2phyml.pl H2Aaln1.fa.trim49_294 
     if (!-e "$treeDir/$file.phyml") { $command .= "fasta2phyml.pl $file ; "; }
+    
+    ### construct the phyml command
     if (!-e "$treeDir/$file.phyml_phyml_tree.txt") { 
+        my $phymlCommand;
         my $logfile = "$file.phyml.log.txt";
-        $command .= "phyml -i $file.phyml -d $alnType --sequential -m $subModel $phymlOptions > $logfile";
-        if ($bootstrap > 0) { $command .= " -b $bootstrap"; }
-        $command .= " ; "; 
+        if ($numThreads == 1) {
+            $phymlCommand .= "phyml ";
+        } else {
+            $phymlCommand .= "mpirun -n $numThreads --oversubscribe phyml-mpi ";
+        }
+        
+        $phymlCommand .= " -i $file.phyml -d $alnType --sequential -m $subModel $phymlOptions > $logfile";
+        if ($bootstrap > 0) { $phymlCommand .= " -b $bootstrap"; }
+        if ($numThreads > 1) { 
+            # need to double-escape if we're using sbatch:
+            if ($use_sbatch == 1) { 
+                $phymlCommand = "/bin/bash -c \\\"source /app/lmod/lmod/init/profile; module load OpenMPI; $phymlCommand ; module purge\\\"";
+            } else {
+                $phymlCommand = "/bin/bash -c \"source /app/lmod/lmod/init/profile; module load OpenMPI; $phymlCommand ; module purge\"";
+            }
+        }
+        ## add $phymlCommand to $command
+        $command .= "$phymlCommand ; ";
     }
     $command .= "changenamesinphyliptreefileguessaliasfilename.pl $file.phyml_phyml_tree.txt ; ";
     if ($removeBranchLengths == 1) {
